329f599b8c4489c3aa2588fe582f315d6540d261
DereviankoYaroslav <yarik0009258@gmail.com>
2022-11-16 20:16:01 +0200

    reduxToolkit cakeStore

---
+++ index.js
@@ -0,0 +1,20 @@
+const store = require('./app/store');
+const cakeActions = require('./features/cake/cakeSlice').cakeActions;
+const iceCreamActions = require('./features/icecream/iceCreamSlice').iceCreamActions;
+
+console.log('IS', store.getState());
+
+const unsubscribe = store.subscribe(() => {
+});
+
+store.dispatch(cakeActions.ordered());
+store.dispatch(cakeActions.ordered());
+store.dispatch(cakeActions.ordered());
+store.dispatch(cakeActions.restocked(3));
+
+store.dispatch(iceCreamActions.ordered());
+store.dispatch(iceCreamActions.ordered());
+store.dispatch(iceCreamActions.ordered());
+store.dispatch(iceCreamActions.restocked(4));
+
+unsubscribe();
